2d40baca52ac2d4ea900c1a9d56a08354e468d9a
Andri Wandres <35061384+AndriWandres@users.noreply.github.com>
2019-03-26 15:38:21 +0100

    add stopwatch.ino

---
+++ stopwatch.ino
@@ -0,0 +1,35 @@
+#include <TimerOne.h>
+#include <Wire.h>
+#include <MultiFuncShield.h>
+
+float timer = 0;
+byte timerStarted = false;
+
+void setup() {
+  Serial.begin(9600);
+  Timer1.initialize();
+  MFS.initialize(&Timer1);
+  MFS.write(0);
+}
+
+void loop() {
+  byte button = MFS.getButton();
+  byte buttonAction = button & B11000000;
+  
+  if (button && buttonAction == BUTTON_SHORT_RELEASE_IND) {
+    // Toggle blinking display
+    MFS.blinkDisplay(DIGIT_ALL, timerStarted ? ON : OFF);
+    
+    // Start/Stop timer
+    timerStarted = !timerStarted;
+  }
+
+  if (timerStarted) {
+    // Increase timer by 100ms
+    delay(100);
+    MFS.write(timer += 0.1);
+  } else {
+    // Reset timer
+    timer = 0;
+  }
+}
